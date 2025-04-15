e0b12500411bb1f7f587944487e254bbab50a629
Harman Rawal <harmanrawal@gmail.com>
2025-04-15 15:37:32 +0530

    Playwright test directory updated

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -4,6 +4,7 @@ const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
 
 export default defineConfig({
   timeout: 60000,
+  testDir: './tests',
   use: {
     baseURL: baseUrl,
     headless: true,
